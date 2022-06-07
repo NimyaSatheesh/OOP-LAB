0eeff8034a27cb73e00d54953b476eff4690075a
Nimya Satheesh <94552601+NimyaSatheesh@users.noreply.github.com>
2022-06-07 13:01:57 +0530

    Add files via upload

---
+++ HashMapDemo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class HashMapDemo {
+public static void main(String args[]) {
+Map<String, Integer> hm = new HashMap<String, Integer>();
+hm.put("Anu", new Integer(1));
+hm.put("sinu", new Integer(2));
+hm.put("Jinu", new Integer(3));
+for (Map.Entry<String, Integer> me : hm.entrySet()) {
+System.out.print(me.getKey() + ":");
+System.out.println(me.getValue());
+}
+}
+}
